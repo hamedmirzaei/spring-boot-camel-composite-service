69ebecdd45a5ca5dba6dfdf45cc5e5a517438a63
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-05-19 16:01:18 +0430

    Single Customer-BaseInfo changed to All Customer-BaseInfo

--- ui-service/src/main/java/spring/boot/cloud/uiservice/UIController.java
+++ ui-service/src/main/java/spring/boot/cloud/uiservice/UIController.java
@@ -9,20 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class UIController {
 
     final String CUSTOMERS_URI = "http://localhost:8085/customers";
     final String BASE_INFOS_URI = "http://localhost:8086/baseinfos";
-    final String COMPOSITE_URI = "http://localhost:8080/customerBaseInfos?cid=1";
+    final String COMPOSITE_URI = "http://localhost:8080/customerBaseInfos?cid=";
 
     @GetMapping("/")
     private String handleRequest(Model model) {
 
         RestTemplate restTemplate = new RestTemplate();
-        FullCustomer fullCustomer = restTemplate.getForObject(COMPOSITE_URI, FullCustomer.class);
+
+        ResponseEntity<List<Customer>> customerResponse = restTemplate.exchange(
+                CUSTOMERS_URI,HttpMethod.GET,null,
+                new ParameterizedTypeReference<List<Customer>>(){});
+        List<Customer> customers = customerResponse.getBody();
+
 
 
         ResponseEntity<List<BaseInfo>> baseInfoResponse = restTemplate.exchange(
@@ -30,14 +37,15 @@ public class UIController {
                 new ParameterizedTypeReference<List<BaseInfo>>(){});
         List<BaseInfo> baseInfos = baseInfoResponse.getBody();
 
-        ResponseEntity<List<Customer>> customerResponse = restTemplate.exchange(
-                CUSTOMERS_URI,HttpMethod.GET,null,
-                new ParameterizedTypeReference<List<Customer>>(){});
-        List<Customer> customers = customerResponse.getBody();
+
+        List<FullCustomer> fullCustomers = customers.parallelStream().map(
+                e -> restTemplate.getForObject(COMPOSITE_URI + e.getBaseInfoId(), FullCustomer.class)
+        ).collect(Collectors.toList());
+
 
         model.addAttribute("baseinfos", baseInfos);
         model.addAttribute("customers", customers);
-        model.addAttribute("fullcustomer", fullCustomer);
+        model.addAttribute("fullcustomers", fullCustomers);
 
         return "index";
     }
